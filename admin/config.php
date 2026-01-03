0fda71cc0b5363aa1f2197c6b98f16aa0e1c19f7
Judeecode <dalumiasjude@gmail.com>
2026-01-03 12:11:22 +0800

    updates

--- admin/config.php
+++ admin/config.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 $host = "localhost";            // Typically `localhost` on XAMPP
 $user = "u993466733_ranicmsDB";                 // Default XAMPP MySQL user
 $pass = "raniDBcms123";                     // Default XAMPP MySQL has no password
-$dbname = "u993466733_ranicmsDB";            // Database that stores CMS data
+$dbname = "u993466733_ranicms";            // Database that stores CMS data
 
 // Create connection
 $conn = new mysqli($host, $user, $pass, $dbname);
