e5148fbfdddd23ad744aaad19cfe31bfcdfb1fe8
Judeecode <dalumiasjude@gmail.com>
2026-01-05 22:05:24 +0800

    push

--- admin/config.php
+++ admin/config.php
@@ -1,13 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 // Database configuration for Rani Beauty Clinic CMS
 // Adjust credentials to match your local XAMPP MySQL setup if needed.
 $host = "localhost";            // Typically `localhost` on XAMPP
-$user = "u993466733_ranicmsDB";                 // Default XAMPP MySQL user
-$pass = "raniDBcms123";                     // Default XAMPP MySQL has no password
-$dbname = "u993466733_ranicms";            // Database that stores CMS data
+$user = "root";                 // Default XAMPP MySQL user
+$pass = "";                     // Default XAMPP MySQL has no password
+$dbname = "ranicms";            // Database that stores CMS data
 
 // Create connection
 $conn = new mysqli($host, $user, $pass, $dbname);
